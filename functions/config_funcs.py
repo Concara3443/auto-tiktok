739487a158e8e0804c189c1cdb43f03f5a3e0c72
Guillermo Cortés <Cortesmesas@gmail.com>
2024-11-11 15:18:40 +0100

    Enhance config_funcs.py to create directories for config files and update config_write function to accept content

--- functions/config_funcs.py
+++ functions/config_funcs.py
@@ -1,6 +1,7 @@
 """Module handles config and config writing"""
 from pathlib import Path
 import toml
+import os
 
 CONFIG_DEFAULT_STRING = """
 max_clip_length = 180 # How long each clip should be when rendering video. default 3 mins (counted in seconds).
@@ -12,14 +13,17 @@ add_hastags = false # adds hastags to the video. default false (should only be t
 config_parsed = toml.loads(CONFIG_DEFAULT_STRING)
 #config = toml.load(f="text_files/config.toml")
 
-def config_write(file):
-    """Function writes config"""
+def config_write(file, content):
+    directory = os.path.dirname(file)
+    if not os.path.exists(directory):
+        os.makedirs(directory)
+    
+    # Abre el archivo en modo escritura
     with open(file, "w", encoding="utf-8") as open_file:
-        toml.dump(config_parsed, open_file)
-
-def config_create(path: Path):
-    """Function checks and creates the config file"""
+        open_file.write(content)
+        
+def config_create(path):
     if path.is_file() is False:
-        config_write(path)
+        config_write(path, CONFIG_DEFAULT_STRING)
     config = toml.load(Path("text_files", "config.toml"))
     return config
