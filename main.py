7f7551414df2550c308064678783d60f29d04ddf
Guillermo Cortés <Cortesmesas@gmail.com>
2024-12-14 16:16:26 +0100

    Add command-line argument for disabling auto shutdown in main program

--- main.py
+++ main.py
@@ -1,10 +1,15 @@
 """Main program loop"""
 import sys
 import os
+import argparse
 from functions.misc_functions import start, clear, folder_file_create, paths, wait_for_input
 from functions.choices import single_vid, clear_temp_files, clear_video_files, multiple_vids
 from utils.links import get_trending_videos, get_playlist_videos, save_file
 from functions.config_funcs import config_create
+
+parser = argparse.ArgumentParser(description="Iniciar el bot de Auto TikTok")
+parser.add_argument('-n', '--no-shutdown', action='store_true', help="Desactivar el autoapagado")
+args = parser.parse_args()
 
 folder_file_create()  # Creates folders / files for videos (skips if they already exist)
 config = config_create(paths["config"])
